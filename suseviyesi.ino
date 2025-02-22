8741aa5085489bd9363561ec511d3a6d4b005264
SIRZATS <sirzaterengulsen@gmail.com>
2025-02-22 14:02:55 +0300

    Update suseviyesi.ino

--- suseviyesi.ino
+++ suseviyesi.ino
@@ -1,27 +1,25 @@
-int motorPin = 9; // Motorun bağlandığı pin
-int sensorPin = A0; // Su seviyesi sensörünün bağlandığı analog pin
-int threshold = 500; // Su seviyesi için eşik değeri (örnek değer)
+int motorPin = 9; 
+int sensorPin = A0; 
+int threshold = 500; 
 
 void setup() {
-  pinMode(motorPin, OUTPUT); // Motor pinini çıkış olarak ayarla
-  pinMode(sensorPin, INPUT); // Sensör pinini giriş olarak ayarla
-  Serial.begin(9600); // Seri haberleşmeyi başlat
+  pinMode(motorPin, OUTPUT); 
+  pinMode(sensorPin, INPUT); 
+  Serial.begin(9600); 
 }
 
 void loop() {
-  int sensorValue = analogRead(sensorPin); // Sensör değerini oku
-  Serial.println(sensorValue); // Seri monitöre sensör değerini yazdır
+  int sensorValue = analogRead(sensorPin); 
+  Serial.println(sensorValue); 
   
   if (sensorValue < threshold) {
-    // Su seviyesi düşükse motoru 3 dakika çalıştır, 1 dakika durdur
-    digitalWrite(motorPin, HIGH); // Motoru çalıştır
-    delay(1500); // 3 dakika bekle (180,000 ms)
+    digitalWrite(motorPin, HIGH); 
+    delay(1500); 
     
-    digitalWrite(motorPin, LOW); // Motoru durdur
-    delay(750); // 1 dakika bekle (60,000 ms)
+    digitalWrite(motorPin, LOW); 
+    delay(750); 
   } else {
-    // Su seviyesi yeterliyse motoru durdur
     digitalWrite(motorPin, LOW);
-    delay(1000); // Su seviyesi kontrolünü sık sık yap
+    delay(1000); 
   }
 }
